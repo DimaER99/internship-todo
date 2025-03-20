62db76cc5066c818bd7eac19684451af0f788593
DimaER99 <avstrovskiyl@mail.ru>
2025-03-20 13:10:14 +0300

    Завершил лабораторную работу №4
    * Завершил лабораторную работу №4
    
    * Внёс исправления в 4 работу
    
    * внёс правки
    
    * Внёс правки #3
    
    * Внёс правки #4

---
+++ student_623044/Student.ConsoleTodo/Student.ConsoleTodo/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Student.ConsoleTodo
+{
+    class Program
+    {
+        /// <summary>
+        /// Список задач для хранения и управлением объектами типа Task
+        /// </summary>
+        private static List<Task> TaskList { get; set; } = new List<Task>();
+
+        static void Main(string[] args)
+        {                 
+            while (true)
+            {
+                Console.Clear();
+                ShowInfo();
+
+                string insertValue = Console.ReadLine();
+                int numberTaskInt;
+                Console.Clear();
+                if (int.TryParse(insertValue, out numberTaskInt))
+                {
+                    CallTaskByNumber(numberTaskInt);
+
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: Введите номер для вызова программы");
+                }
+
+                while (true)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Для повторного ввода операции нажмите Enter, для завершения приложения Esc.");
+                    ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                    if (keyInfo.Key == ConsoleKey.Escape)
+                    {
+                        return;
+                    }
+                    if (keyInfo.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        /// <summary>
+        /// Вызов задачи по номеру
+        /// </summary>
+        /// <param name="numberTask">Номер задачи</param>
+        private static void CallTaskByNumber(int numberTask)
+        { 
+            switch (numberTask)
+            {
+                case 1:
+                    Console.WriteLine("1 - Посмотреть список задач");
+                    ViewTaskList();
+
+                    Exit();
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("2 - Добавить задачу");
+                    Task task = new Task().AddTask();
+                    TaskList.Add(task);     
+                    break;
+
+                default:
+                    Console.WriteLine("Некорректный выбор номера");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Показать информацию
+        /// </summary>
+        private static void ShowInfo()
+        {
+            Console.WriteLine("Для вызова выполняемой подпрограммы укажите ее номер и нажмите Enter: ");
+            Console.WriteLine("\r\n1 - Посмотреть список задач");
+            Console.WriteLine("2 - Добавить задачу");
+
+        }
+
+        /// <summary>
+        /// Показать список задач
+        /// </summary>
+        public static void ViewTaskList()
+        {
+            int number = 0; 
+            Console.WriteLine("\r\nСписок задач:");
+            foreach (var task in TaskList)
+            {
+                number += 1;
+                Console.WriteLine($"\r\nЗадача номер: '{number}'");
+                Console.WriteLine(task);
+            }
+        }
+
+        /// <summary>
+        /// Выход в список подпрограмм
+        /// </summary>
+        private static void Exit()
+        {
+            string str = "\r\nДля возврата к списку подпрограмм нажмите Esc: ";
+            Console.WriteLine(str);
+
+            ConsoleKeyInfo key = Console.ReadKey(true);
+
+            if (key.Key == ConsoleKey.Escape)
+            {
+                Console.Clear();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("\r\nОшибка. Для возврата к списку подпрограмм нажмите Esc: ");
+                return;
+            }
+        }
+    }
+}
