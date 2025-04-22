a72cb31c0ab1178d12fe5aaa14c8077d168ca39d
DimaER99 <avstrovskiyl@mail.ru>
2025-04-22 08:13:18 +0300

    Lab8 623044 (#8)
    * Выполнил лабораторную работу №8
    
    * Revert "Выполнил лабораторную работу №8"
    
    This reverts commit 3c543c913c522ac4904710a793847c2399a96910.
    
    * Закинул packages в .gitignore
    
    * Выполнил лабораторную работу №8
    
    * + бд
    
    * Внёс правки #1
    
    * Внёс правки №2

---
+++ student_623044/Student/Student.Todo.Data/TaskService.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Student.Todo.Data
+{
+    public class TaskService
+    {
+        public readonly string connectionStrings;
+
+        public TaskService(string connectionString)
+        {
+            connectionStrings = connectionString;
+        }
+
+        /// <summary>
+        /// Добавить задачу
+        /// </summary>
+        /// <param name="task">Задача</param>
+        public void AddTaskInDataBase(Task task)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionStrings))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(
+                 $"INSERT INTO [dbo].[Todo] (Title, Description) VALUES (@Title, @Description)", connection);
+                command.Connection = connection;
+                command.Parameters.AddWithValue("Title", task.Title);
+                command.Parameters.AddWithValue("Description", task.Description);
+
+                command.ExecuteNonQuery().ToString();
+            }
+        }
+
+        /// <summary>
+        /// Получить задачи
+        /// </summary>
+        /// <returns>Задачи</returns>
+        public DataSet GetTasksFromDataBase()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionStrings))
+            {
+                string query = "SELECT Id, Title, Description FROM Todo";
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection))
+                {
+                    DataSet dataSet = new DataSet();
+                    dataAdapter.Fill(dataSet);
+                    return dataSet;
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Удаление задача
+        /// </summary>
+        /// <param name="idTaskDelete">Ид задачи</param>
+        public void DeleteTaskFromId(int idTaskDelete)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionStrings))
+            {
+                string query = "DELETE FROM Todo WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", idTaskDelete);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Изменить задачу
+        /// </summary>
+        /// <param name="idChangeTask">Ид задачи</param>
+        /// <param name="changeTitle">Изменить заголовок</param>
+        /// <param name="changeDescription">Изменить описание</param>
+        public void ChangeTaskFromId(int idChangeTask, string changeTitle, string changeDescription)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionStrings))
+            {
+                string query = "UPDATE Todo SET Title = @Title, Description = @Description WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    command.Parameters.AddWithValue("@Title", changeTitle);
+                    command.Parameters.AddWithValue("@Description", changeDescription);
+                    command.Parameters.AddWithValue("@Id", idChangeTask);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
--- student_623044/Student/Student.WebFormsTodo/Default.aspx
+++ student_623044/Student/Student.WebFormsTodo/Default.aspx
@@ -4,11 +4,34 @@
 
     <div>
         <h1>Список задач</h1>
-        <asp:HiddenField runat="server" ID="hfSelectIndex" />
         <asp:Button ID="buttonAdd" runat="server" CssClass="btn btn-primary mb-2" OnClick="bAddTask_OnClick" Text="Добавить задачу" />
         <div>
-            <asp:GridView ID="gvTask" runat="server" AutoGenerateColumns="false" OnRowDeleting="gvTask_OnRowDeleting" OnRowEditing="gvTask_OnRowEditing" AllowPaging="true" CssClass="table table-bordered">
+            <asp:GridView ID="gvTask" runat="server" AutoGenerateColumns="false"
+                AllowPaging="true"
+                CssClass="table table-bordered"
+                DataKeyNames="Id"
+                OnRowDeleting="gvTask_RowDeleting"
+                OnRowEditing="gvTask_OnRowEditing"
+                OnPageIndexChanging="gvTask_PageIndexChanging">
+
+                <PagerTemplate>
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination pagination-sm">
+                            <li class="page-item">
+                                <asp:LinkButton ID="lnkPrev" runat="server" CommandName="Page" CommandArgument="Prev"
+                                    CssClass="page-link">Назад</asp:LinkButton>
+                            </li>
+                            <asp:PlaceHolder ID="phPages" runat="server"></asp:PlaceHolder>
+                            <li class="page-item">
+                                <asp:LinkButton ID="lnkNext" runat="server" CommandName="Page" CommandArgument="Next"
+                                    CssClass="page-link">Вперед</asp:LinkButton>
+                            </li>
+                        </ul>
+                    </nav>
+                </PagerTemplate>
+
                 <Columns>
+                    <asp:BoundField DataField="Id" HeaderText="ID" ReadOnly="true" />
                     <asp:BoundField DataField="Title" HeaderText="Заголовок" ReadOnly="true" />
                     <asp:BoundField DataField="Description" HeaderText="Описание" ReadOnly="true" />
                     <asp:ButtonField ButtonType="Button" HeaderText="Действие" CommandName="Edit" Text="Изменить" />
@@ -89,7 +112,7 @@
                     <h5>Если вы действительно хотите удалить задачу нажмите "Удалить", иначе "Отмена".</h5>
                 </div>
                 <div class="modal-footer">
-                    <asp:LinkButton runat="server" class="btn btn-primary" OnClick="lbEditDeleteTask_OnClick">Удалить</asp:LinkButton>
+                    <asp:LinkButton ID="lbDeleteRow" runat="server" class="btn btn-primary" OnClick="lbDeleteRow_Click">Удалить</asp:LinkButton>
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Отмена</button>
                 </div>
             </div>
